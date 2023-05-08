127eb1703dece5b50f7e4b3d96e5e54890ff0d88
zbahati <bahatizirimwabagabo@gmai.com>
2023-05-08 07:11:39 +0200

    Add webpack so that i can run date in deployemnet

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import { DateTime } from 'luxon';
 import BookList from './modules/constructors.js';
 import { showBooks } from './modules/navigation.js';
+
+const dt = DateTime.local();
+const formattedDate = dt.toFormat('MMMM dd yyyy, h:mm:ss a');
+
+const time = document.getElementById('date_time');
+time.innerHTML = ` ${formattedDate}`;
 
 const lists = new BookList();
 lists.display();
--- modules/datetime.js
+++ modules/datetime.js
@@ -1,6 +1,6 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+// import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-const time = document.getElementById('date_time');
-const dt = DateTime.local();
-const formattedDate = dt.toFormat("MMMM dd'th 'yyyy', 'hh:mm:ss a");
-time.innerHTML = ` ${formattedDate}`;
+// const time = document.getElementById('date_time');
+// const dt = DateTime.local();
+// const formattedDate = dt.toFormat("MMMM dd'th 'yyyy', 'hh:mm:ss a");
+// time.innerHTML = ` ${formattedDate}`;
